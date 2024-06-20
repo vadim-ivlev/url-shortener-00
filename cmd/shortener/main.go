9cb6882535fac12fe567f22e76b4c81f6163d4c5
Vadim Ivlev <vadimivlev@Vadims-Laptop.local>
2024-06-20 01:42:45 +0100

    add server.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+
+	// Создать мультиплексор
+	mux := http.NewServeMux()
+
+	// Добавить обработчик для корневого URL
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request). {
+		method := r.Method
+		// Эндпоинт с методом `POST`
+		if method == "POST" {
+			shortenedURL := getShortenedURL(r)
+
+		w.Write([]byte("Hello, World! " + method + "\n"))
+	})
+
+	// Запустить сервер на порту 8080
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+
+}
